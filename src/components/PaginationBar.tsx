76025ce0fa0cda4065ad0e9dae17e53c9dbb420a
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-10-14 12:05:40 +0530

    pagination implemented

---
+++ src/components/PaginationBar.tsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+
+type PaginationBarProps = {
+  currentPage: number;
+  totalPages: number;
+};
+
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationBarProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+
+  const numberedPageItems: JSX.Element[] = [];
+  for (let page = minPage; page < maxPage; page++) {
+    numberedPageItems.push(
+      <Link
+        href={`?page=${page}`}
+        key={page}
+        className={`join-item btn ${
+          currentPage === page ? "btn-active pointer-events-none" : ""
+        }`}
+      >
+        {page}
+      </Link>
+    );
+  }
+
+  return (
+    <>
+      <div className="join  flex items-center justify-center sm:hidden">
+        {currentPage > 1 && (
+          <Link href={`?page=${currentPage - 1}`} className="join-item btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+              />
+            </svg>
+          </Link>
+        )}
+        <button className="join-item btn pointer-events-none">
+          Page {currentPage}
+        </button>
+        {currentPage < totalPages && (
+          <Link href={`?page=${currentPage + 1}`} className="join-item btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </Link>
+        )}
+      </div>
+      <div className="join hidden sm:block">{numberedPageItems}</div>
+    </>
+  );
+}
