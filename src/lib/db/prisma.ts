619c7d9d348c0c2a42a2006c58114f16f240d6f5
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-10-14 12:34:22 +0530

    cart relations optimized

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -4,8 +4,19 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prismaBase = globalForPrisma.prisma ?? new PrismaClient();
+
+export const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      },
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prismaBase;
 }
